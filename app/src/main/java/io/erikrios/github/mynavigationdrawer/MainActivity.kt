0849687b389dca8a4cbdad31192d2550ee2650a1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-30 11:23:38 +0700

    Add an id

--- app/src/main/java/io/erikrios/github/mynavigationdrawer/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mynavigationdrawer/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_cart
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
